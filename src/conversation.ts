d3ed7f161b7da41dd65f4eb70dda67af9c57a37f
kejun <kejun1997@gmail.com>
2023-02-17 22:45:35 +0800

    feat: use https to replace ofetch

--- src/conversation.ts
+++ src/conversation.ts
@@ -1,22 +1,45 @@
 import { Conversation, Cookie } from "./types";
-import { ofetch } from "ofetch";
 import { createHeaders, stringifyCookie } from "./utils";
 
+import https from "https";
+import crypto from "crypto";
+
 export const createConversation = async (cookie: Cookie) => {
-  const headers = {
-    ...createHeaders(),
-    cookie: stringifyCookie(cookie),
-  };
-  console.log(headers)
-  const result = await ofetch<Conversation>(
-    "https://edgeservices.bing.com/edgesvc/turing/conversation/create",
-    {
-      headers,
-    }
-  );
-  if (result.result.value !== "Success") {
-    throw result;
-  }
-  result.headers = headers;
-  return result;
+  return new Promise((resolve, reject) => {
+    let response = "";
+    const headers = {
+      ...createHeaders(),
+      cookie: stringifyCookie(cookie),
+    };
+
+    const defaultCiphers = crypto.constants.defaultCoreCipherList.split(":");
+    const shuffledCiphers = [
+      defaultCiphers[0],
+      defaultCiphers[2],
+      defaultCiphers[1],
+      ...defaultCiphers.slice(3),
+    ].join(":");
+
+    https
+      .get(
+        "https://edgeservices.bing.com/edgesvc/turing/conversation/create",
+        {
+          headers,
+          ciphers: shuffledCiphers,
+        },
+        (res) => {
+          res.on("data", (chunk: Buffer) => {
+            response += chunk.toString("utf-8");
+          });
+          res.on("end", () => {
+            const data = JSON.parse(response) as Conversation;
+            if (data.result.value !== "Success") {
+              reject(data);
+            }
+            resolve(data);
+          });
+        }
+      )
+      .on("error", reject);
+  });
 };
